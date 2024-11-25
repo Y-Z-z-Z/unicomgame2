a8550511f983892584cbc11aaa4a9e51b16e6f64
YZZ <549223656@qq.com>
2024-11-25 21:15:59 +0800

    Replace next.config.ts with next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/unicomgame2'
+}
+
+module.exports = nextConfig
